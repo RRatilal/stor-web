bc099a4d3542d87f1e6ef9abea5bc0499a414a5c
rratilal <rachidratilal@gmail.com>
2021-09-01 21:40:11 +0200

    Update ScheduleProvider

--- src/context/schedule.tsx
+++ src/context/schedule.tsx
@@ -11,9 +11,56 @@ interface IScheduleContext {
 const ScheduleContext = createContext({} as IScheduleContext)
 
 const ScheduleProvider: React.FC = ({ children }) => {
-    const { updatedUser } = useAdmin()
-    const [subject, setSubject] = useState("");
-    const [chosedTimes, setChoseTimes] = useState([{}]);
+    const { updatedUser } = useAdmin();
+    const [selected, setSelected] = useState("");
+    const [clickedItem, setClickedItem] = useState<number | null>(null)
+    const [chosenTimeItem, setChosenTimeItem] = useState([
+        {chosenTime: "", day: ""}
+    ]);
+
+    useEffect(() => {
+        console.log(chosenTimeItem)
+    }, [chosenTimeItem])
+
+    function getScheduleData(event: MouseEvent<HTMLDivElement>, day: string, index: number) {
+        event.stopPropagation();
+
+        const currentDiv = event.currentTarget
+
+        const findTime = chosenTimeItem.find(time => time.chosenTime === currentDiv.innerHTML)
+
+        const date = new Date();
+        const currentWeekday = date.getDay();
+        const currentDay = date.getDate();
+
+        
+        // if (currentWeekday === 3 && schedule?.week_day === 4) {
+        //     console.log(schedule?.week_day)
+        // }
+
+        setClickedItem(index)
+
+        if (!findTime) {
+            setChosenTimeItem([
+                ...chosenTimeItem,
+                {chosenTime: currentDiv.innerHTML, day}
+            ]);
+
+            // currentDiv.className = "hourMinutes selected"
+
+            setSelected("selected");
+
+            // currentDiv.className = `hourMinutes ${selected}`;
+        } else {
+            const removingTime = chosenTimeItem.filter(time => time.chosenTime !== findTime.chosenTime)
+
+            console.log("removing time", removingTime)
+
+            setChosenTimeItem(removingTime)
+
+            // currentDiv.className = "hourMinutes"
+
+            setSelected("");
 
     function getScheduleData() {
         console.log(updatedUser)
